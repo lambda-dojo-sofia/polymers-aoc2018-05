542e20692ce1dc9d3b382d3f0455b72d81998a5e
Evgeni Kolev <evgenysw@gmail.com>
2018-12-11 09:39:02 +0200

    Second haskell solution

--- haskell/reaction.hs
+++ haskell/reaction.hs
@@ -6,6 +6,8 @@ polarity x = if isLower x then toUpper x else toLower x
 canReact :: Char -> Char -> Bool
 canReact x y = x == polarity y
 
+-- first solution, using recursion until loop stops returning a different result
+
 loop [] = []
 loop (x:y:ys) = if canReact x y then loop ys else [x]++(loop ([y]++ys))
 loop (x:xs) = [x]
@@ -13,4 +15,9 @@ loop (x:xs) = [x]
 solve xs = let solution = loop xs
            in if solution == xs then solution else solve solution
 
+-- second solution using foldr:
+
+solve2 xs = foldr f' [] xs
+f' x [] = [x]
+f' x acc = if canReact (head acc) x then tail acc else x:acc
 
